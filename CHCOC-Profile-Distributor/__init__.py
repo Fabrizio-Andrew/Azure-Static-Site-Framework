912a4ac255f2614c28a2acfc104bca2fc242eade
Andrew <andrewdfabrizio@gmail.com>
2021-05-06 15:01:54 -0400

    Python function reads uploaded file content

--- CHCOC-Profile-Distributor/__init__.py
+++ CHCOC-Profile-Distributor/__init__.py
@@ -6,4 +6,6 @@ import azure.functions as func
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+                 f"Blob Size: {myblob.length} bytes\n"
+                 f"Content: {myblob.read()}")
+
