3feef2375933b2d57d4876a20d32fb8493846eac
Andrew <andrewdfabrizio@gmail.com>
2021-05-12 20:06:14 -0400

    implemented csv parsing in Profile Distributor

--- CHCOC-Profile-Distributor/__init__.py
+++ CHCOC-Profile-Distributor/__init__.py
@@ -1,4 +1,6 @@
 import logging
+from io import StringIO
+import csv
 
 import azure.functions as func
 
@@ -6,6 +8,13 @@ import azure.functions as func
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes\n"
-                 f"Content: {myblob.read()}")
+                 f"Blob Size: {myblob.length} bytes\n")
 
+    body = myblob.read().decode("utf-8")
+    logging.info(f"raw body: {body}")
+
+    csv_reader = csv.reader(body.split('\n'), delimiter='|')
+    for row in csv_reader:
+        logging.info(row)
+    parsed_csv = list(csv_reader)
+    logging.info(f"parsed csv: {parsed_csv}")
