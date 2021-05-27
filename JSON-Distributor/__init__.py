4c63d2141a1308b279891f49c3c602540d2361ed
Andrew <andrewdfabrizio@gmail.com>
2021-05-27 10:29:45 -0400

    init.py successfully consumes csv

--- JSON-Distributor/__init__.py
+++ JSON-Distributor/__init__.py
@@ -1,10 +1,12 @@
 import logging
 from io import StringIO
+import os
 import csv
 import json
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from .validation import Validate_Member
+from.configsettings import ConfigSettings
 import uuid
 
 
@@ -14,7 +16,10 @@ def main(myblob: func.InputStream):
                  f"Blob Size: {myblob.length} bytes\n")
 
     # Create the BlobServiceClient
-    blob_service_client = BlobServiceClient.from_connection_string()
+    blob_service_client = BlobServiceClient.from_connection_string(ConfigSettings.STORAGE_CONNECTION_STRING)
+
+    # Create required containers (if they don't already exist)
+    CreateContainersIfNotExist(ConfigSettings.STORAGE_CONNECTION_STRING, blob_service_client)
 
     # Extract the csv body from stream
     body = myblob.read().decode("utf-8")
@@ -38,6 +43,7 @@ def main(myblob: func.InputStream):
         # Validate the resulting dict
         errors = Validate_Member(row_dict)
         if len(errors) > 0:
+
             error_id = uuid.uuid1()
             logging.info(f"Error ID: {error_id}\n"
                         "Error Creating file. Check failed files container for details.")
@@ -46,10 +52,46 @@ def main(myblob: func.InputStream):
             with open(f"{error_id}", "w") as outfile:
                 json.dump(error_dict, outfile)
         else:
-            # Create file
-            with open(f"{row_dict['agency_abbrev']}-{row_dict['title']}-{row_dict['first_name']}-{row_dict['last_name']}.json", 'w') as outfile:
-                json.dump(row_dict, outfile)
+            # Create a file name and body
+            filename = f"{row_dict['agency_abbrev']}-{row_dict['title']}-{row_dict['first_name']}-{row_dict['last_name']}.json"
+            file_body = json.dumps(row_dict, indent = 4)
 
+            # Write JSON data to the file
+            file = open(filename, 'w')
+            file.write(file_body)
+            file.close()
+
+            # Create the blob client
+            blob_client = blob_service_client.get_blob_client(container=ConfigSettings.JSON_FILES_CONTAINERNAME, blob=filename)
+
+            # Upload the file
+            with open(filename, 'rb') as outfile:
+                blob_client.upload_blob(outfile)
             
             logging.info(f"File Created: {row_dict['agency_abbrev']}-{row_dict['title']}-{row_dict['first_name']}-{row_dict['last_name']}.json")
 
+"""
+Creates the JSON, ERROR, and ARCHIVE containers if they do not already exist.
+"""
+def CreateContainersIfNotExist(connection_string, blob_service_client):
+
+    # Create the JSON container (if it doesn't already exist)
+    json_container = ContainerClient.from_connection_string(connection_string, ConfigSettings.JSON_FILES_CONTAINERNAME)
+    try:
+        container_properties = json_container.get_container_properties()
+    except Exception as e:
+        json_container = blob_service_client.create_container(ConfigSettings.JSON_FILES_CONTAINERNAME)
+
+    # Create the error container (if it doesn't already exist)
+    error_container = ContainerClient.from_connection_string(connection_string, ConfigSettings.ERROR_FILES_CONTAINERNAME)
+    try:
+        container_properties = error_container.get_container_properties()
+    except Exception as e:
+        json_container = blob_service_client.create_container(ConfigSettings.ERROR_FILES_CONTAINERNAME)
+
+    # Create the archive container (if it doesn't already exist)
+    archive_container = ContainerClient.from_connection_string(connection_string, ConfigSettings.ARCHIVE_CONTAINERNAME)
+    try:
+        container_properties = archive_container.get_container_properties()
+    except Exception as e:
+        json_container = blob_service_client.create_container(ConfigSettings.ARCHIVE_CONTAINERNAME)
